645067b449d3bc94968b5586c3844cfc348d7c67
Paige Niedringhaus <paige_e_niedringhaus@homedepot.com>
2018-08-03 09:57:45 -0400

    adding in action mapping

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,17 +1,18 @@
 import React from 'react';
 // how you grab data using redux instead of passing it through props
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 /* by placing the toggle message action in another file, it can be removed
 from the dispatch function here (that's an 'action creator') and it can
  then be reused and dropped anywhere in your project */
 import { toggleMessage } from './actions';
 
-const Toggle = ({ messageVisibility, dispatch }) => (
+const Toggle = ({ messageVisibility, toggleMessage }) => (
   <div>
     {messageVisibility && (
       <p>You will be seeing this if redux action is toggled</p>
     )}
-    <button type="button" onClick={() => dispatch(toggleMessage())}>
+    <button type="button" onClick={toggleMessage}>
       Toggle Me
     </button>
   </div>
@@ -22,5 +23,19 @@ const mapStateToProps = state => ({
   messageVisibility: state.message.messageVisibility,
 });
 
+/* now instead of having to import dispatch,
+we can just assign the onclick to toggleMessage instead of the function,
+multiple actions can be added here as well */
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      toggleMessage,
+    },
+    dispatch,
+  );
+
 // connects toggle to redux store and export all of it as Toggle
-export default connect(mapStateToProps)(Toggle);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Toggle);
